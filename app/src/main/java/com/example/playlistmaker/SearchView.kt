bc5ca63735ddd104a130d2984d7236f3ba444a3e
Siustoster <gameover666999@yandex.ru>
2026-05-31 23:22:18 +0300

    Финальный комит по заданию 9 спринта.

--- app/src/main/java/com/example/playlistmaker/SearchView.kt
+++ app/src/main/java/com/example/playlistmaker/SearchView.kt
@@ -1,20 +1,70 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SearchView : AppCompatActivity() {
+    private var editTextString: String = EDIT_TEXT_DEF
+
+    companion object {
+        const val EDIT_TEXT_NAME = "SEARCH_EDIT_TEXT"
+        const val EDIT_TEXT_DEF = ""
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(EDIT_TEXT_NAME, editTextString)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        editTextString = savedInstanceState.getString(EDIT_TEXT_NAME, EDIT_TEXT_DEF)
+        val editText = findViewById<EditText>(R.id.search_edit_text)
+        editText.setText(editTextString)
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search_view)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val clearButton = findViewById<ImageView>(R.id.clear_search_button)
+        val editText = findViewById<EditText>(R.id.search_edit_text)
+        val backButton = findViewById<ImageButton>(R.id.search_back_button)
+
+        backButton.setOnClickListener {
+            finish()
         }
+        clearButton.setOnClickListener {
+            editText.text.clear()
+            editText.clearFocus()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(
+                editText.applicationWindowToken,
+                0
+            ) //что бы скрыть клавиатуру по тз. Просто очистка фокуса не помогала.
+        }
+        val simpleTextWatcher: TextWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                editTextString = s.toString()
+                if (s.isNullOrEmpty()) {
+                    clearButton.visibility = View.INVISIBLE
+                } else clearButton.visibility = View.VISIBLE
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        }
+        editText.addTextChangedListener(simpleTextWatcher)
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,14 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -17,9 +20,24 @@ class SettingsActivity : AppCompatActivity() {
         val appShareButton = findViewById<LinearLayout>(R.id.appShareButton)
         val supportButton = findViewById<LinearLayout>(R.id.supportButton)
         val licenceAgreementButton = findViewById<LinearLayout>(R.id.licenceAgreementButton)
+        val themeSwitch = findViewById<SwitchMaterial>(R.id.theme_switch)
+
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        val initialSwitchState = when (currentNightMode) {
+            Configuration.UI_MODE_NIGHT_NO -> false
+            Configuration.UI_MODE_NIGHT_YES -> true
+            else -> false
+        }
+        themeSwitch.isChecked = initialSwitchState
+        themeSwitch.setOnCheckedChangeListener { button, isChecked ->
+            if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
         backButton.setOnClickListener {
             finish()
         }
+
         appShareButton.setOnClickListener {
             val shareIntent = Intent(Intent.ACTION_SEND)
             shareIntent.type = "text/plain"
@@ -29,6 +47,7 @@ class SettingsActivity : AppCompatActivity() {
             )
             startActivity(shareIntent)
         }
+
         supportButton.setOnClickListener {
             val supportIntent = Intent(Intent.ACTION_SENDTO)
             supportIntent.data = Uri.parse("mailto:" + getString(R.string.developer_email))
@@ -42,6 +61,7 @@ class SettingsActivity : AppCompatActivity() {
             )
             startActivity(supportIntent)
         }
+
         licenceAgreementButton.setOnClickListener {
             val licenceIntent = Intent(Intent.ACTION_VIEW)
             licenceIntent.data = Uri.parse(getString(R.string.license_agreement_link))
